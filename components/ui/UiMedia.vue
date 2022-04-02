af2f2bd8430f59432d2227ba1c66c136a9ac4d61
Michel Chouinard <michel.chouinard@gmail.com>
2022-04-02 13:03:00 -0400

    Initial commit

---
+++ components/ui/UiMedia.vue
@@ -0,0 +1,124 @@
+<script>
+import classnames from "classnames";
+
+export const UiMedia = {
+  props: {
+    gap: {
+      default: "m",
+      type: String
+    },
+    tag: {
+      default: "div",
+      type: String
+    }
+  },
+  render() {
+    const Tag = this.tag;
+    return (
+      <Tag class={classnames("media", `media--gap-${this.gap}`)}>
+        {this.$slots.default}
+      </Tag>
+    );
+  }
+};
+
+export const UiMediaFigure = {
+  props: {
+    align: {
+      default: "start",
+      type: String
+    },
+    tag: {
+      default: "div",
+      type: String
+    }
+  },
+  render() {
+    const Tag = this.tag;
+    return (
+      <Tag
+        class={classnames(
+          "media__figure",
+          `media__figure--align-${this.align}`
+        )}
+      >
+        {this.$slots.default}
+      </Tag>
+    );
+  }
+};
+
+export const UiMediaBody = {
+  props: {
+    align: {
+      default: "start",
+      type: String
+    },
+    tag: {
+      default: "div",
+      type: String
+    }
+  },
+  render() {
+    const Tag = this.tag;
+    return (
+      <Tag
+        class={classnames("media__body", `media__body--align-${this.align}`)}
+      >
+        {this.$slots.default}
+      </Tag>
+    );
+  }
+};
+
+export default UiMedia;
+</script>
+
+<style lang="scss">
+$gap-m: 1em;
+$gap-l: 2em;
+
+.media {
+  display: flex;
+}
+
+.media__figure {
+  &--align-center {
+    align-self: center;
+  }
+
+  &--align-end {
+    align-self: flex-end;
+  }
+
+  &:first-child {
+    .media--gap-m > & {
+      margin-right: $gap-m;
+    }
+
+    .media--gap-l > & {
+      margin-right: $gap-l;
+    }
+  }
+
+  &:last-child {
+    .media--gap-m > & {
+      margin-left: $gap-m;
+    }
+
+    .media--gap-l > & {
+      margin-left: $gap-l;
+    }
+  }
+}
+
+.media__body {
+  &--align-center {
+    align-self: center;
+  }
+
+  &--align-end {
+    align-self: flex-end;
+  }
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,73 @@
+<template>
+  <UiGrid>
+    <UiGridItem :width="['12/12', '4/12@m']">
+      <h2>The Sidebar</h2>
+      <p>Some sidebar content ...</p>
+    </UiGridItem>
+    <UiGridItem :width="['12/12', '8/12@m']">
+      <h2>Main Content</h2>
+      <UiMedia>
+        <UiMediaFigure>
+          <img
+            src="https://images.unsplash.com/photo-1510798831971-661eb04b3739?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=70&h=70&q=80"
+            alt=""
+          />
+        </UiMediaFigure>
+        <UiMediaBody>
+          Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
+          nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat,
+          sed diam voluptua.
+        </UiMediaBody>
+      </UiMedia>
+      <br />
+      <UiMedia>
+        <UiMediaBody>
+          Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
+          nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat,
+          sed diam voluptua.
+        </UiMediaBody>
+        <UiMediaFigure>
+          <img
+            src="https://images.unsplash.com/photo-1510798831971-661eb04b3739?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=70&h=70&q=80"
+            alt=""
+          />
+        </UiMediaFigure>
+      </UiMedia>
+      <br />
+      <UiMedia>
+        <UiMediaBody>
+          Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
+          nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat,
+          sed diam voluptua. Lorem ipsum dolor sit amet, consetetur sadipscing
+          elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore
+          magna aliquyam erat, sed diam voluptua.
+        </UiMediaBody>
+        <UiMediaFigure align="center">
+          <img
+            src="https://images.unsplash.com/photo-1510798831971-661eb04b3739?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=70&h=70&q=80"
+            alt=""
+          />
+        </UiMediaFigure>
+      </UiMedia>
+    </UiGridItem>
+  </UiGrid>
+</template>
+
+<script>
+import { UiGrid, UiGridItem } from "../components/ui/UiGrid.vue";
+import {
+  UiMedia,
+  UiMediaBody,
+  UiMediaFigure
+} from "../components/ui/UiMedia.vue";
+
+export default {
+  components: {
+    UiGrid,
+    UiGridItem,
+    UiMedia,
+    UiMediaBody,
+    UiMediaFigure
+  }
+};
+</script>
